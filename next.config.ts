5ccf03d8cb89d1061fc5b2afb665e6ae9a8d040c
Robert Vollmer <dev@robv.de>
2026-03-10 21:33:38 +0000

    Fix compiled server

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  transpilePackages: ['@mapbox/mapbox-gl-draw', 'mapbox-gl-draw-split-polygon-mode', 'apbox-gl-draw-passing-mode'],
+  transpilePackages: ['@mapbox/mapbox-gl-draw', 'mqtt'],
 };
 
 export default nextConfig;
